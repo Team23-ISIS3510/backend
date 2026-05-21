faa387c4a480cc93eef8301e32277786bd9a693c
kmiloquimbay <d.quimbay@uniandes.edu.co>
2026-05-21 14:51:16 -0500

    add course import

--- src/modules/tutor/tutor-application.types.ts
+++ src/modules/tutor/tutor-application.types.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
+import { Course } from '../academic/entities/course.entity';
 
 export class TutorApplicationDto {
   @ApiProperty({ example: 'tutor-123', description: 'Tutor ID' })
